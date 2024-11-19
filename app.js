1b5fd8e90171e5d80725ff2e38d883c5f6e01ed4
J Abbott <jabbott@Jshaconas-MacBook.local>
2024-11-18 22:26:25 -0400

    Implementation for AJAX section

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-document.getElementById('Search Button').addEventListener('click', function(){
-const url = "superheroes.php?action=getSuperheroes";
-fetch(url)
-.then(response => response.json())
-.then(superheroes => {
-let superheroNames = superheroes.map(hero => hero.name).join(', ');
-alert("Superheroes: " + superheroNames);
-})
-.catch(error => {
-alert("An error occurred.");
+document.getElementById("searchButton").addEventListener("click", function () {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "superheroes.php", true);
+
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            alert(xhr.responseText);
+        } else {
+            alert("Error fetching superhero information.");
+        }
+    };
+
+    xhr.send();
 });
-});
+
+
